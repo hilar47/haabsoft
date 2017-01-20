e8196ca54eb05f2f2b8955edea5907042cae3c25
Pranay Vernekar <pranay@CCI-MACBK-56.local>
2017-01-20 22:25:54 +0530

    Added the functions to hide posts from specific user roles. And also removed all the counts.

--- wp-content/themes/Royal_Theme-v2.8/functions.php
+++ wp-content/themes/Royal_Theme-v2.8/functions.php
@@ -15,3 +15,29 @@ function admin_style() {
 	wp_enqueue_style('font-awsome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css");
 }
 add_action('admin_enqueue_scripts', 'admin_style');
+
+//Restrict users to their own posts
+function posts_for_current_author($query) {
+	global $pagenow;
+
+	if( 'edit.php' != $pagenow || !$query->is_admin )
+	    return $query;
+
+	if( !current_user_can( 'edit_others_posts' ) ) {
+		global $user_ID;
+		$query->set('author', $user_ID );
+	}
+	return $query;
+}
+add_filter('pre_get_posts', 'posts_for_current_author');
+
+//hide all count
+add_filter( 'views_edit-post', 'wpse149143_edit_posts_views' );
+
+function wpse149143_edit_posts_views( $views ) {
+    foreach ( $views as $index => $view ) {
+        $views[ $index ] = preg_replace( '/ <span class="count">\([0-9]+\)<\/span>/', '', $view );
+    }
+
+    return $views;
+}
