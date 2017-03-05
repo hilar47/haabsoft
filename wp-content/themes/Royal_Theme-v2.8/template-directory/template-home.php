9bec406c33ed4ae7674d5c7c7690d76898c1d43b
Hilario Goes <hilariogoes47@gmail.com>
2017-03-05 11:09:16 +0530

    Done new changes.

--- wp-content/themes/Royal_Theme-v2.8/template-directory/template-home.php
+++ wp-content/themes/Royal_Theme-v2.8/template-directory/template-home.php
@@ -5,10 +5,20 @@ Template Name: Home
 get_header();
 $get_country_name = json_decode(file_get_contents("http://freegeoip.net/json/"));
 ?>
-<div>
 	<?php /*?><?php echo do_shortcode('[rev_slider alias="poster"]');?><?php */?>
-    <img src="<?php echo site_url() . '/wp-content/themes/Royal_Theme-v2.8/images/custom-assets/banner.png'; ?>" alt="Sign up as viwer" class="center-block" />
-</div>
+    <div class="homepage-hero-module">
+		<div class="video-container">
+			<div class="filter"></div>
+			<video autoplay loop class="fillWidth">
+				<source src="wp-content/themes/Royal_Theme-v2.8/videos/cover.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+				<source src="wp-content/themes/Royal_Theme-v2.8/videos/cover.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
+			</video>
+			<div class="poster hidden">
+				<img src="wp-content/themes/Royal_Theme-v2.8/videos/cover.jpg" alt="">
+			</div>
+		</div>
+	</div>
+
 <div class="container home">
 		<div class="row">
 		<?php
@@ -32,66 +42,71 @@ $get_country_name = json_decode(file_get_contents("http://freegeoip.net/json/"))
 			</div>
 		</div>
 		<!-- end row-fluid -->
-		
-		<!-- Row Backgrounds -->
-				<div class="wpb_wrapper videos">
-										<?php /*?><h2><?php echo (isset($meta['video_section_title']) && !empty($meta['video_section_title']) ? $meta['video_section_title'] : '');?></h2>
-										<hr class="divider break">
-										<p style="font-size: 18px;"><?php echo (isset($meta['video_section_description']) && !empty($meta['video_section_description']) ? $meta['video_section_description'] : '');?></p><?php */?>
-										<?php
-										//echo $_POST['countryKey'];
-										//for a given post type, return all
-										$post_type = 'videos';
-										$tax = 'videos_category';
-										$tax_terms = get_terms($tax);
-										if ($tax_terms) {
-										  foreach ($tax_terms  as $tax_term) {
-										    $args=array(
-										      'post_type' => $post_type,
-										      "$tax" => $tax_term->slug,
-										      'post_status' => 'publish',
-										      'posts_per_page' => -1,
-										      'caller_get_posts'=> 1,
-										      'meta_query' => array(
-													array(
-										                'key' => $content_item_meta->get_the_name('country'),
-										                'value'=> $get_country_name->country_name
-											        )
-												)
-										    );
+</div>
 
-										    $my_query = null;
-										    $my_query = new WP_Query($args);
-										    if( $my_query->have_posts() ) { ?>
-										    <h3><?php if(isset($tax_term->name) && !empty($tax_term->name)) { echo $tax_term->name; } ?></h3>
-										    	<div class="row">
-										    	<?php
-										      while ($my_query->have_posts()) : $my_query->the_post(); 
-										      	global $content_item_meta;
-										      	$video_meta = get_post_meta($post->ID, $content_item_meta->get_the_id(), true);
-										            ?>
-										        
-										        <div class="col-xs-12 col-sm-4 m-b-20">
-										        	<video class="img-responsive" controls><source src="<?php if(isset($video_meta['video_upload']) && !empty($video_meta['video_upload'])) { echo $video_meta['video_upload']; } ?>" type="video/mp4"></video>
-													<div class="video-disc">
-														<h5>Lorem ipsum dolor sit amet, consectetur</h5>
-													<p>Uploaded by : Rohit Raul</p>
-													</div>
-										        	
-										        </div>
-										        <?php
-										           
-										      endwhile;
-										      ?>
-										      </div>
-										      <?php
-										    }
-										    wp_reset_query();
-										  }
-										}
-										?>
-									</div>
-								</div>
+<!-- Videos -->
+		<div class="wpb_wrapper videos">
+            <h3>FEATURED VIDEOS</h3>
+            <div class="container">
+                <?php /*?><h2><?php echo (isset($meta['video_section_title']) && !empty($meta['video_section_title']) ? $meta['video_section_title'] : '');?></h2>
+                <hr class="divider break">
+                <p style="font-size: 18px;"><?php echo (isset($meta['video_section_description']) && !empty($meta['video_section_description']) ? $meta['video_section_description'] : '');?></p><?php */?>
+                <?php
+                //echo $_POST['countryKey'];
+                //for a given post type, return all
+                $post_type = 'videos';
+                $tax = 'videos_category';
+                $tax_terms = get_terms($tax);
+                if ($tax_terms) {
+                  foreach ($tax_terms  as $tax_term) {
+                    $args=array(
+                      'post_type' => $post_type,
+                      "$tax" => $tax_term->slug,
+                      'post_status' => 'publish',
+                      'posts_per_page' => -1,
+                      'caller_get_posts'=> 1,
+                      'meta_query' => array(
+                            array(
+                                'key' => $content_item_meta->get_the_name('country'),
+                                'value'=> $get_country_name->country_name
+                            )
+                        )
+                    );
+
+                    $my_query = null;
+                    $my_query = new WP_Query($args);
+                    if( $my_query->have_posts() ) { ?>
+                    <h3><?php if(isset($tax_term->name) && !empty($tax_term->name)) { echo $tax_term->name; } ?></h3>
+                        <div class="row">
+                        <?php
+                      while ($my_query->have_posts()) : $my_query->the_post(); 
+                        global $content_item_meta;
+                        $video_meta = get_post_meta($post->ID, $content_item_meta->get_the_id(), true);
+                            ?>
+
+                        <div class="col-xs-12 col-sm-4 m-b-20">
+                            <video class="img-responsive" controls><source src="<?php if(isset($video_meta['video_upload']) && !empty($video_meta['video_upload'])) { echo $video_meta['video_upload']; } ?>" type="video/mp4"></video>
+                            <div class="video-disc">
+                                <h5>Lorem ipsum dolor sit amet, consectetur</h5>
+                            <p>Uploaded by : Rohit Raul</p>
+                            </div>
+
+                        </div>
+                        <?php
+
+                      endwhile;
+                      ?>
+                      </div>
+                      <?php
+                    }
+                    wp_reset_query();
+                  }
+                }
+                ?>
+                </div>
+            </div>
+
+
 							</div>
 						</div>
 					</div>
@@ -100,7 +115,6 @@ $get_country_name = json_decode(file_get_contents("http://freegeoip.net/json/"))
 				<p></p>
 				<div class="post-navigation">
 				</div>
-				<p class="edit-link"><a class="post-edit-link" href="<?php echo site_url();?>wp-admin/post.php?post=22879&amp;action=edit">Edit this</a> <a href="<?php echo site_url();?>/wp-admin/post.php?vc_action=vc_inline&amp;post_id=22879&amp;post_type=page" id="vc_load-inline-editor" class="vc_inline-link">Edit with Visual Composer</a></p>
 			</div>
 		</div>
 		<!-- end row-fluid -->
